571134919d8d53a8e55c59f08a1945dcfa6a47a0
anaaanyev <anaaanyev@gmail.com>
2026-06-11 09:49:25 +0300

    feat: PostForm empty_label for fieds (which is --------- by default)

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -5,7 +5,7 @@ from blog_app.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'author', 'category']
+        fields = ['title', 'author', 'category', 'content']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
@@ -18,6 +18,18 @@ class PostForm(forms.ModelForm):
             'author': 'Автор',
             'category': 'Категория',
         }
+
+    # https://docs.djangoproject.com/en/6.0/ref/forms/fields/#modelchoicefield
+    # https://www.reddit.com/r/djangolearning/comments/urnmgr/django_forms_empty_label_attribute_for_select/
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # print(self.fields)
+        # {'title': <django.forms.fields.CharField object at 0x1066f1580>,
+        # 'author': <django.forms.models.ModelChoiceField object at 0x1066f14f0>,
+        # 'category': <django.forms.models.ModelChoiceField object at 0x1064cf7a0>,
+        # 'content': <django.forms.fields.CharField object at 0x1062cd100>}
+        self.fields['author'].empty_label = "Выберите автора"
+        self.fields['category'].empty_label = "Выберите категорию"
 
     def clean_title(self):
         title = self.cleaned_data.get('title')
