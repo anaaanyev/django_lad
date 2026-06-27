04f1999a4a7dfa7388ca35e2ef0eea19b1938d7e
anaaanyev <anaaanyev@gmail.com>
2026-06-27 12:06:36 +0300

    Lesson is done

--- users_app/apps.py
+++ users_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersAppConfig(AppConfig):
     name = "users_app"
     verbose_name = "Профили пользователей"
+
+    def ready(self):
+        import users_app.signals # noqa: F401
--- users_app/forms.py
+++ users_app/forms.py
@@ -8,16 +8,18 @@ from users_app.models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["bio", "social_link"]
+        fields = ["bio", "social_link", "avatar"]
 
         widgets = {
             "bio": forms.Textarea(attrs={'class': 'form-control'}),
-            "social_link": forms.URLInput(attrs={'class': 'form-control'})
+            "social_link": forms.URLInput(attrs={'class': 'form-control'}),
+            "avatar": forms.ClearableFileInput(attrs={'class': 'form-control'})
         }
 
         labels = {
             'bio': 'Расскажите о себе',
             'social_link': 'Ваша ссылка на соцсеть',
+            'avatar': 'Аватар'
         }
 
 
