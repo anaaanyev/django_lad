ac20dbe61a55aede727bd8d033f69020f95ddc18
anaaanyev <anaaanyev@gmail.com>
2026-07-06 11:30:35 +0300

    feat: REST API для категорий

--- drf_app/urls.py
+++ drf_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from drf_app.api import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
+from drf_app.api import (
+    PostListCreateAPIView,
+    PostRetrieveUpdateDestroyAPIView,
+    CategoryListCreateAPIView,
+    CategoryRetrieveUpdateDestroyAPIView
+)
 
 app_name = 'drf'
 
 urlpatterns = [
     path('posts/', PostListCreateAPIView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-detail'),
+    path('categories/', CategoryListCreateAPIView.as_view(), name='categories-list-create'),
+    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-detail'),
 ]
